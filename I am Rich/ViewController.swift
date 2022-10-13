1bbabd456c76f65f20403c31681633ba5336e6f7
Alex Zavadskiy <fh.zavadskii@gmail.com>
2022-10-13 22:05:49 +0300

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alex Zavadskiy on 17.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
